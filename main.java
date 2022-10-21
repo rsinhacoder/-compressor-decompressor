a6cf8bf1ee6b334dc869a193f0de9a9cbd8cd371
Ritik Sinha <84030322+rsinhacoder@users.noreply.github.com>
2022-10-21 21:35:02 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package GUI;
+
+/**
+ *
+ * @author shivenduac
+ */
+public class main {
+    
+    public static void main(String[] args){
+        AppFrame frame = new AppFrame();
+    }
+    
+}
